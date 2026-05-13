4354cb67aa5ac96a76e8a60c4a090f2bf4900c64
Daniela Murai <dany.murai@gmail.com>
2026-05-12 21:08:31 -0300

    incluindo readme e retirando comentários do codigo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Desafio Aula 09 - PGATS
+
+Este projeto é um desafio proposto na Aula 09 da disciplina PGATS. O objetivo é criar uma classe em JavaScript que utiliza objetos e listas, além de implementar testes para essa classe.
+
+## Estrutura do Projeto
+
+- `src/servicoDePagamentoDeEmpresas.js`: Implementação da classe principal.
+- `test/servicoDePagamentoDeEmpresas.test.js`: Arquivo de testes.
+
+## Como executar os testes
+
+Certifique-se de ter o Node.js instalado. Execute `npm test` para rodar os testes.
--- src/servicoDePagamentoDeEmpresas.js
+++ src/servicoDePagamentoDeEmpresas.js
@@ -1,19 +1,3 @@
-/*
-  Crie uma classe que possua dois métodos: um para realizar pagamento e outro para consultar o último pagamento. Pagamentos serão armazenados como objetos Javascript dentro de uma lista de pagamentos. Cada pagamento terá as propriedades: Código de Barras, Empresa e Valor. Quando o pagamento for realizado e o valor for maior que 100.00, o pagamento também terá a propriedade categoria como 'cara'. Caso contrário, a propriedade categoria ficará como 'padrão'. O método de consultar trará apenas o último pagamento. 
-  
-  Ex.
-  const servicoDePagamento = new ServicoDePagamento();
-  servicoDePagemento.pagar('1231-4563-7895', 'Samar', 56.87);
-  console.log(servicoDePagamento.consultaUltimoPagamento());
-  {
-    codigoBarras: '1231-4563-7895',
-    empresa: 'Samar',
-    valor: 56.87
-  }
-  
-  A entrega deve ser realizada via Github e o repositório deve ter a classe a pasta src e os testes dos métodos dessa classe dentro da pasta teste usando Mocha e Node Assert.
-*/
-
 export default class ServicoDePagamentoDeEmpresas {
   #pagamentos
   
@@ -43,11 +27,5 @@ export default class ServicoDePagamentoDeEmpresas {
 }
   
 const servicoDePagamento = new ServicoDePagamentoDeEmpresas();
-// servicoDePagamento.realizarPagamento('3265-4895-7452', 'Claro', 150.95);
-// servicoDePagamento.realizarPagamento('3265-4895-7452', 'Vivo', 50.95);
-// servicoDePagamento.realizarPagamento('3265-4895-7452', 'Tim', 250.95);
-// servicoDePagamento.realizarPagamento('3265-4895-7452', 'Tim', );
-
-//console.log(servicoDePagamento.consultar())
   
 
